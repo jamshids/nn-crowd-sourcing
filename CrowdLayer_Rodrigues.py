44187cfde3aed24a6c5f6118aa274552dd95d13d
Jamshid Sourati <jamshid.sourati@childrens.harvard.edu>
2019-04-09 16:34:48 -0400

    Merging all the crowd layers into a single tensor

--- CrowdLayer_Rodrigues.py
+++ CrowdLayer_Rodrigues.py
@@ -1,6 +1,7 @@
 from itertools import tee
 import tensorflow as tf
 import numpy as np
+import pdb
 import sys
 import os
 
@@ -12,35 +13,39 @@ import NN_extended
 
 class model(object):
 
-    def __init__(self, model, sess, K):
+    def __init__(self, model, sess, K, name):
 
         self.K = K
         self.c = model.class_num
         self.model = model
         self.sess = sess
-        # CL: crowd layers
-        self.CL_dict = {}
+        self.name = name
 
-        # constructing crowd layers
-        for k in range(self.K):
-            CL = NN_extended.CNN(self.model.posteriors, {}, 
-                                 self.model.name+'_CL_{}'.format(k))
-            with tf.variable_scope(CL.name):
-                W_CL = NN_extended.weight_variable('Weight', 
-                                                   [self.c, self.c],
-                                                   CL.regularizer,
-                                                   CL.custom_getter)
-                CL.output = tf.matmul(W_CL, CL.output)
-                if len(CL.dropout_layers) > 0:
-                    CL.output = tf.nn.dropout(CL.output, CL.keep_prob)
-                    
-                # update the prediction and posterior ops
-                CL.posteriors = tf.transpose(
-                    tf.nn.softmax(tf.transpose(CL.output)))
-                CL.prediction = tf.argmax(CL.posteriors, 0)
-                CL.var_dict['last'] = [W_CL]
+    def build_crowd_layers(self):
+        """Modeling all the crowd layers in a single tensor weight
+        """
 
-                self.CL_dict['CL_{}'.format(k)] = CL
+        shape_W = [self.K, self.c, self.c]
+        shape_b = [self.K, self.c, 1]
+        with tf.variable_scope(self.name, reuse=tf.AUTO_REUSE):
+            # parameters of the crowd layers
+            init_W = np.stack([np.eye(self.c, dtype=np.float32) 
+                               for k in range(self.K)], axis=0)
+            self.crowd_W = tf.get_variable('crowd_W', initializer=init_W)
+            init_b = tf.constant(0., shape=shape_b)
+            self.crowd_b = tf.get_variable('crowd_b', initializer=init_b)
+
+            # output of the crowd layers 
+            # (K x 2 x b)
+            self.crowd_output = tf.keras.backend.dot(
+                self.crowd_W, self.model.posteriors) + self.crowd_b
+            self.crowd_posteriors = tf.nn.softmax(self.crowd_output,
+                                                  axis=1)
+            
+            # get the target placeholder for the crowds
+            self.crowd_target = tf.placeholder(tf.float32, 
+                                               self.crowd_output.shape)
+
 
     def get_optimizer(self, optimizer_name='SGD', 
                       trace_reg=False,
@@ -51,32 +56,19 @@ class model(object):
         in all the crowd layers.
         """
 
-        self.CL_losses = {}
+        with tf.variable_scope(self.name):
 
-        with tf.variable_scope(self.model.name):
-
-            if trace_reg:
-                self.tr_reg_lambda = tf.placeholder(tf.float32)
-            else:
-                self.tr_reg_lambda = tf.constant(0.)
-
+            self.global_step = tf.Variable(0, 
+                                           trainable=False, 
+                                           name='global_step')
             # the loss
-            self.masked_y = []
-            self.masked_output = []
-            for k in range(self.K):
-                CL = self.CL_dict['CL_{}'.format(k)]
-
-                # filter one-hot label placeholder
-                y_mask = tf.not_equal(tf.reduce_sum(CL.y_, axis=0),0)
-                self.masked_y += [tf.boolean_mask(CL.y_, y_mask, axis=1)]
-                self.masked_output += [tf.boolean_mask(CL.output, y_mask, axis=1)]
-
-                self.CL_losses['CL_{}'.format(k)] = tf.nn.softmax_cross_entropy_with_logits_v2(
-                    labels=tf.transpose(self.masked_y[k]), 
-                    logits=tf.transpose(self.masked_output[k])) + \
-                    self.tr_reg_lambda * tf.trace(CL.var_dict['last'][0])
-
-            self.loss = tf.reduce_sum([tf.reduce_mean(loss) for _,loss in self.CL_losses.items()])
+            vec = tf.nn.softmax_cross_entropy_with_logits_v2(
+                logits=self.crowd_output,
+                labels=self.crowd_target,
+                dim=1)            
+            mask = tf.equal(self.crowd_target[:,0,:], -1)
+            zer = tf.zeros_like(vec)
+            self.loss = tf.where(mask, x=zer, y=vec)
 
             # the optimizer
             if optimizer_name=='SGD':
@@ -86,29 +78,24 @@ class model(object):
 
             # the train step
             # variables of the main body
-            tr_vars = [v for _,v in self.model.var_dict.items()] 
-            # variables of the crowd layers
-            tr_vars += [[v for _,v in MM.var_dict.items()] 
-                        for _,MM in self.CL_dict.items()]
-
-            grads_vars = self.optimizer.compute_gradients(self.loss, tr_vars)
+            train_vars = [V[i] for _,V in self.model.var_dict.items()
+                          for i in range(len(V)) if V[i] in 
+                          tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)]
+            train_vars += [self.crowd_W, self.crowd_b]
+            self.grads_vars = self.optimizer.compute_gradients(self.loss, train_vars)
 
             update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
             with tf.control_dependencies(update_ops):
-                self.train_op = self.optimizer.apply_gradients(
-                    grads_vars, global_step=self.model.global_step)
+                self.train_step = self.optimizer.apply_gradients(
+                    self.grads_vars, global_step=self.global_step)
         
 
-    def initialize(self, identity_crowd_layer=True):
+    def initialize_crowd_model(self):
         """Initializing parameters and optimization variables
         """
 
-        self.model.initialize(self.sess)
+        # collect all the optimization parameters
+        var_list = [V for V in tf.global_variables() if 
+                    self.name in V.name]
+        self.sess.run(tf.variables_initializer(var_list))
 
-        if identity_crowd_layer:
-            # initialize the matrix weights of crowd layers 
-            # with identities
-            for k in range(self.K):
-                self.sess.run(tf.assign(
-                    self.CL_dict['CL_{}'.format(k)].var_dict['last'][0], 
-                    np.eye(self.c)))
